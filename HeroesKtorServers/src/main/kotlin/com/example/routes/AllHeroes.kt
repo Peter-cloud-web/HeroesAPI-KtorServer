5ed68755a3d1db6ad58f69f500ea053b309fdf50
Peter-cloud-web <petergichia165@gmail.com>
2023-08-30 11:52:55 +0300

    Implement AllHeroes endpoint and handle all usecases as far as exceptions are concerned

--- HeroesKtorServers/src/main/kotlin/com/example/routes/AllHeroes.kt
+++ HeroesKtorServers/src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,7 +1,36 @@
 package com.example.routes
 
+import com.example.models.ApiResponse
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.get
 
-fun Route.getAllHeroes(){
+fun Route.getAllHeroes() {
+    get("/anime/heroes") {
+
+        try {
+
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5)
+            call.respond(message = page)
+
+
+        } catch (e: NumberFormatException) {
+
+            call.respond(
+                message = ApiResponse(success = false, message = "Only numbers allowed"),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e:IllegalArgumentException){
+
+            call.respond(
+                message = ApiResponse(success = false, message = "Heroes not found"),
+                status = HttpStatusCode.BadRequest
+            )
+        }
+
+    }
 
 }
