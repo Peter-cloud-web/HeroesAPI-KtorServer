0cdb0e06db5cd2a471a6b361c0c60db47ec5081e
Peter-cloud-web <petergichia165@gmail.com>
2023-08-30 11:09:30 +0300

    define a Koin module to inject the necessary dependencies

--- HeroesKtorServers/src/main/kotlin/com/example/plugins/Koin.kt
+++ HeroesKtorServers/src/main/kotlin/com/example/plugins/Koin.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.di.koinModule
 import io.ktor.server.application.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
@@ -8,5 +9,7 @@ import org.koin.logger.slf4jLogger
 fun Application.configureKoin(){
     install(Koin){
       slf4jLogger()
+        modules(koinModule)
+
     }
 }
