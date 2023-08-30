74e80e4eb4ece8733b2103de9cfcc10ba0de0fc4
Peter-cloud-web <petergichia165@gmail.com>
2023-08-30 15:43:20 +0300

    Inject repository dependency with koin

--- HeroesKtorServers/src/main/kotlin/com/example/routes/AllHeroes.kt
+++ HeroesKtorServers/src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,20 +1,29 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.get
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
-    get("/anime/heroes") {
 
+    val heroRepository: HeroRepository by inject()
+
+    get("/anime/heroes") {
         try {
 
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
-            call.respond(message = page)
+
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
 
 
         } catch (e: NumberFormatException) {
