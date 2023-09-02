7d387b2fb6b4d4a3b5d6c6c06b2e1ccc67619a6c
Peter-cloud-web <petergichia165@gmail.com>
2023-09-02 10:27:01 +0300

    Implement search heroes functionality

--- HeroesKtorServers/src/main/kotlin/com/example/repository/HeroRepository.kt
+++ HeroesKtorServers/src/main/kotlin/com/example/repository/HeroRepository.kt
@@ -14,5 +14,7 @@ interface HeroRepository {
     val page5: List<Hero>
 
     suspend fun getAllHeroes(page: Int = 1): ApiResponse
-    suspend fun searchHeroes(name: String): ApiResponse
+    suspend fun searchHeroes(name: String?): ApiResponse
+
+
 }
--- HeroesKtorServers/src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
+++ HeroesKtorServers/src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
@@ -417,26 +417,30 @@ class HeroRepositoryImpl:HeroRepository {
             1->prevPage = null
             5->nextPage = null
         }
-
-//        if(page in 1..4){
-//            nextPage = nextPage?.plus(1)
-//        }
-//        if(page in 2..5){
-//            prevPage = prevPage?.minus(1)
-//        }
-//        if(page == 1){
-//            prevPage = null
-//        }
-//        if(page == 5){
-//            nextPage = null
-//        }
         return mapOf(PREVIOUS_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
     }
 
-    override suspend fun searchHeroes(name: String): ApiResponse {
-        TODO("Not yet implemented")
+    override suspend fun searchHeroes(name: String?): ApiResponse {
+        return ApiResponse(
+            success = true,
+            message = "ok",
+            heroes = findHeroes(query = name)
+        )
     }
 
-
-
+    private fun findHeroes(query: String?):List<Hero> {
+        val founded = mutableListOf<Hero>()
+        return if (!query.isNullOrBlank()) {
+            heroes.forEach { (_, heroes) ->
+                heroes.forEach { hero ->
+                    if (hero.name.lowercase().contains(query.lowercase())) {
+                        founded.add(hero)
+                    }
+                }
+            }
+            founded
+        }else{
+            emptyList()
+        }
+    }
 }
